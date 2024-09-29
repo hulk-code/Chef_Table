97547f460dd41ccaad3d5ce419ee9cd22cd27563
unknown <shoaibsarkarakash019@gmail.com>
2024-09-30 00:26:24 +0600

    Create Awesome Order Now Button

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,27 +1,32 @@
-import banner from '../../assets/Banner/Banner.webp'
+import banner from "../../assets/Banner/Banner.webp";
+import { IoFastFood } from "react-icons/io5";
+import './Banner.css'
 
 const Banner = () => {
-    return (
-        <div>
-           <div
-  className="hero min-h-screen"
-  style={{
-    backgroundImage: `url(${banner})`,
-  }}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div> 
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url(${banner})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello foodie's</h1>
+            <p className="mb-5 flex items-center gap-4 lg:ml-10 ">
+              Savor the Flavors of Our Culinary Delights.
+              <IoFastFood className="text-5xl"></IoFastFood>
+              <span></span>
+            </p>
+            <button className='glowing-btn'><span className='glowing-txt'>Ord
+                <span className='faulty-letter'>er</span>Now</span></button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
