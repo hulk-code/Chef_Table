234cb1884e74b0ce48963a51ae6a4a762ae720ad
unknown <shoaibsarkarakash019@gmail.com>
2024-09-30 00:32:00 +0600

    add titel icon

--- src/Component/Navber/Navbar.jsx
+++ src/Component/Navber/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <div className="navbar-container ">
           <img src={logo} alt="My Logo" className="navbar-logo" /> 
           <ul className="navbar-links ">
-           <div className='flex items-center lg:gap-x-10 lg:mr-96 font-bold'>
+           <div className='flex items-center md:gap-x-10 lg:gap-x-10 lg:mr-96 font-bold'>
            <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
